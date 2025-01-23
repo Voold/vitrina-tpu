dab4bb337ce57665d8c45248d9825eba953ea8be
Voold <barsik.koteevich@mail.ru>
2025-01-24 05:21:27 +0700

    repair BrowserRoutes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter} from 'react-router-dom';
 ReactDOM.render(<React.StrictMode>  
-      <HashRouter>
+      <BrowserRouter>
         <App />
-      </HashRouter>
+      </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 )
